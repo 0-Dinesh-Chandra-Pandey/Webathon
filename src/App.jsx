1327e0ef75ae56f4f5d159cd24845d7ae648152d
0-Dinesh-Chandra-Pandey <dchandrap973@gmail.com>
2024-09-30 20:17:26 +0530

    "Basic Things"

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home/Home'
+import Careers from './Pages/Careers/Careers'
+import About from './Pages/About/About'
+import Contact from './Pages/Contact/Contact'
+import Services from './Pages/Services/Services'
+import Incubation from './Pages/Incubation/Incubation'
+import Events from './Pages/Events/Events'
+import Resources from './Pages/Resources/Resources'
+
+function App() {
+
+  return (
+    <>
+      {/* HOME PAGE */}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/careers' element={<Careers />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/events' element={<Events />} />
+        <Route path='/incubation' element={<Incubation />} />
+        <Route path='/resources' element={<Resources />} />
+        <Route path='/services' element={<Services />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
