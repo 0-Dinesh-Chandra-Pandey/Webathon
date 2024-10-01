5fc49e9a8570db5e4e1cfe7773eb981a72b2e471
0-Dinesh-Chandra-Pandey <dchandrap973@gmail.com>
2024-10-01 13:44:40 +0530

    added some code

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
 import Footer from "../../Components/Footer/Footer";
-import "./About.css"
-import {aboutData} from "../../Data/about.js"
+import "./About.css";
+import { aboutData } from "../../Data/about.js";
 
 const About = () => {
     return (
@@ -10,35 +10,21 @@ const About = () => {
             <Navbar />
 
             <div>
-                {
-                  aboutData.map((item) => {
+                {aboutData.map((item) => {
                     return (
-                      <div className="main">
-                    <div className="Image">
-                        <img className="img" src={item.imgSrc} />
-                    </div>
-                    <div className="Content">
-                        <h2 className="Heading">Our Mission</h2>
-                        <p className="text-about">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit. Similique culpa laudantium reiciendis, neque
-                            minima asperiores eaque fugit totam unde sunt
-                            laboriosam veniam consequuntur eius debitis alias
-                            reprehenderit! Iste ullam optio nesciunt beatae
-                            aliquid soluta enim tenetur delectus suscipit!
-                            Rerum, ab. Lorem ipsum, dolor sit amet consectetur
-                            adipisicing elit. Officiis temporibus doloribus
-                            quasi voluptatem dolorum, at nisi dicta tenetur
-                            similique. Nostrum, minus! Esse perferendis commodi
-                            est itaque, eaque dignissimos non aliquid. Deleniti
-                            ipsum, illum quae excepturi debitis explicabo,
-                            magnam exercitationem molestiae
-                        </p>
-                    </div>
-                </div>
-                    ) 
-                  })
-                }
+                        <div className="main">
+                            <div className="Image">
+                                <img className="img" src={item.imgSrc} />
+                            </div>
+                            <div className="Content">
+                                <h2 className="Heading">{item.title}</h2>
+                                <p className="text-about">
+                                    {item.description}
+                                </p>
+                            </div>
+                        </div>
+                    );
+                })}
             </div>
             <Footer />
         </div>
--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
-import Navbar from '../../Components/Navbar/Navbar'
-import Footer from '../../Components/Footer/Footer'
+import React from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import Footer from "../../Components/Footer/Footer";
+import "./services.css";
+import image from "../../assets/services/Service-1.png";
+import { servicesCardData } from "../../Data/services.js";
 
 const Services = () => {
-  return (
-    <div className='container'>
-      <Navbar />
-      <Footer />
-      
-    </div>
-  )
-}
+    return (
+        <div className="container">
+            <Navbar />
 
-export default Services
+            <div className="content-container">
+                {servicesCardData.map((item) => {
+                    return (
+                        <div className="content-wrapper">
+                            <div className="left-section">
+                                <img src={item.imgSrc} alt="" srcset="" />
+                            </div>
+
+                            <div className="right-section">
+                                <div className="heading">
+                                    <h1>{item.heading}</h1>
+                                    <span className="bar"></span>
+                                </div>
+
+                                <p>
+                                    {item.description}
+                                </p>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+
+            <Footer />
+        </div>
+    );
+};
+
+export default Services;
